c981b67e85f72c348c5e5bdd0d9a6799f9222c56
Kritsana <tragoolphrom@gmail.com>
2018-07-24 16:12:47 +0700

    Demo Lotto
    Signed-off-by: Kritsana <tragoolphrom@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { MembershipPage } from '../pages/membership/membership';
+import { TradePage } from '../pages/trade/trade';
+import { CollectionPage } from '../pages/collection/collection';
+import { TicketPage } from '../pages/ticket/ticket';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    MembershipPage,
+    TradePage,
+    CollectionPage,
+    TicketPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +27,11 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    MembershipPage,
+    TradePage,
+    CollectionPage,
+    TicketPage
   ],
   providers: [
     StatusBar,
